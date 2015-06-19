9b32c77c6128e58e3918655b07b6d3a41472844e
Peter Neubauer <development@smeky.de>
2015-06-19 14:40:26 +0200

    SuperClassClassSuper

--- photobooth/client/__main__.py
+++ photobooth/client/__main__.py
@@ -5,7 +5,9 @@ import subprocess
 import sys
 
 import gphoto2 as gp
-import showPhoto
+
+from tkinter import *
+from PIL import ImageTk, Image
 
 def main():
     context = gp.gp_context_new()
@@ -33,7 +35,20 @@ def main():
     
     print('Open image in gui')
     
-    showPhoto(target)
+    root = Tk()
+    #root.overrideredirect(True)
+
+    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+    root.geometry("%dx%d+0+0" % (w, h))
+
+    originalImg = Image.open(target)
+    resizedImg = originalImg.resize((w, h),Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(resizedImg)
+
+    panel = Label(root, image = img)
+    panel.pack(side = "bottom", fill = "both", expand = "yes")
+
+    root.mainloop()
     
     return 0
 
