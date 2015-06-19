cbfa081f81a8cadb77d00374f8a30bff308c8673
Peter Neubauer <development@smeky.de>
2015-06-19 14:20:48 +0200

    Take photo with python

--- photobooth/client/__main__.py
+++ photobooth/client/__main__.py
@@ -1,9 +1,36 @@
+
+
+import os
+import subprocess
+import sys
+
 import gphoto2 as gp
-context = gp.gp_context_new()
-error, camera = gp.gp_camera_new()
-error = gp.gp_camera_init(camera, context)
-error, text = gp.gp_camera_get_summary(camera, context)
-print('Summary')
-print('=======')
-print(text.text)
-error = gp.gp_camera_exit(camera, context)
+
+def main():
+    context = gp.gp_context_new()
+    camera = gp.check_result(gp.gp_camera_new())
+    gp.check_result(gp.gp_camera_init(camera, context))
+ 
+    print('Capturing image')
+    
+    file_path = gp.check_result(gp.gp_camera_capture(
+        camera, gp.GP_CAPTURE_IMAGE, context))
+    
+    print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+    
+    target = os.path.join('/tmp', file_path.name)
+    
+    print('Copying image to', target)
+    
+    camera_file = gp.check_result(gp.gp_camera_file_get(
+            camera, file_path.folder, file_path.name,
+            gp.GP_FILE_TYPE_NORMAL, context))
+    
+    gp.check_result(gp.gp_file_save(camera_file, target))
+    
+    gp.check_result(gp.gp_camera_exit(camera, context))
+    
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
