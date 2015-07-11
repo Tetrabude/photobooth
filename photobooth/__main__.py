a7c5f0bb105c64eaa84149da9cf64faa9d705965
pi <pi@smeky.de>
2015-07-11 13:30:18 +0200

    Changed import statements

--- photobooth/__main__.py
+++ photobooth/__main__.py
@@ -6,22 +6,21 @@ import sys
 
 
 
-
-from photobooth.gui import gui
+import gui
 
 try:
     import gphoto2 as gp
     print("gphoto2 loaded")
-    from photobooth.client.camera import camera as camera
+    import client.camera as camera
 except ImportError:
     print("cameraMock loaded")
-    from photobooth.client.cameraMock import cameraMock as camera
+    import client.cameraMock as camera
 
 
 def main():
     print('Main rutine started')
-    cam = camera()
-    g = gui(cam)
+    cam = camera.camera()
+    g = gui.gui(cam)
 
     g.run()
     
