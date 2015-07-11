53dd53245f90473b110b28721cabc7bced1b70e6
Peter Neubauer <development@smeky.de>
2015-07-11 11:01:20 +0200

    Added flickr upload

---
+++ photobooth/server/flickr.py
@@ -0,0 +1,39 @@
+# http://stuvel.eu/media/flickrapi-docs/documentation/
+
+import flickrapi
+import webbrowser
+
+api_key = u'9460c8c076928bf8239504139c1e8c88'
+api_secret = u'042eb1bdb5531c1f'
+
+flickr = flickrapi.FlickrAPI(api_key, api_secret)
+
+print('Step 1: authenticate')
+
+# Only do this if we don't have a valid token already
+if not flickr.token_valid(perms='write'):
+
+    # Get a request token
+    flickr.get_request_token(oauth_callback='oob')
+
+    # Open a browser at the authentication URL. Do this however
+    # you want, as long as the user visits that URL.
+    authorize_url = flickr.auth_url(perms='write')
+    webbrowser.open_new_tab(authorize_url)
+
+    # Get the verifier code from the user. Do this however you
+    # want, as long as the user gives the application the code.
+    verifier = input('Verifier code: ')
+#
+    # Trade the request token for an access token
+    flickr.get_access_token(verifier)
+
+print('Step 2: use Flickr')
+
+
+
+filename = "bild.jpg"
+
+resp = flickr.upload(filename)
+
+print(resp)
