938ad88f4d5cf986cac9a8c649ef5fc3d38af093
Peter Neubauer <development@smeky.de>
2015-07-13 15:03:23 +0200

    Unicode Fuckup

--- photobooth/server/flickrConnect.py
+++ photobooth/server/flickrConnect.py
@@ -15,22 +15,22 @@ class flickrConnect():
     def checkAuthentication(self):
         print('Check Flickr Authentification')
         # Only do this if we don't have a valid token already
-        if not self.flickr.token_valid(perms='write'):
+        if not self.flickr.token_valid(perms=u'write'):
     
             # Get a request token
-            self.flickr.get_request_token(oauth_callback='oob')
+            self.flickr.get_request_token(oauth_callback=u'oob')
     
             # Open a browser at the authentication URL. Do this however
             # you want, as long as the user visits that URL.
-            authorize_url = self.flickr.auth_url(perms='write')
+            authorize_url = self.flickr.auth_url(perms=u'write')
             webbrowser.open_new_tab(authorize_url)
     
             # Get the verifier code from the user. Do this however you
             # want, as long as the user gives the application the code.
-            verifier = input('Verifier code: ')
-    #
+            verifier = unicode(raw_input('Verifier code: '))
+            #
             # Trade the request token for an access token
-            self.flickr.get_access_token(verifier)
+            self.flickr.get_access_token(uVerifier)
             
             print('Authentification successfull')
         else:
