30d793ffac1da96e76e1e62c2d355b2d35997795
Peter Neubauer <development@smeky.de>
2015-07-13 12:52:23 +0200

    Changed to pixel Layout

--- photobooth/gui.py
+++ photobooth/gui.py
@@ -15,8 +15,8 @@ class gui():
         
         print('Gui initialized')
 
-        #self.w, self.h = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
-        self.w, self.h = 1200, 800
+        self.w, self.h = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
+        #self.w, self.h = 1200, 800
         self.root.geometry("%dx%d+0+0" % (self.w, self.h))
         
         self.loadButtons()
@@ -29,9 +29,9 @@ class gui():
         
     def loadLogo(self):
         originalLogo = Image.open("Kontingentsabzeichen.png")
-        originalLogo = originalLogo.resize(( int(self.h*0.27), int(self.h*0.27) ),Image.ANTIALIAS)
+        originalLogo = originalLogo.resize(( 175, 175 ),Image.ANTIALIAS)
         self.logo = ImageTk.PhotoImage(originalLogo)
-        Tkinter.Label(self.root, image=self.logo).grid(row=1, column=1, rowspan=3, padx=5, pady=5)
+        Tkinter.Label(self.root, image=self.logo).place(x=self.w - 180, y= self.h - 180)
         
     def loadImage(self):
         self.photoPath = self.cam.takePhoto()
@@ -39,24 +39,24 @@ class gui():
         (originalImgWidth, originalImgHeight) = originalImg.size
         imgProportion = originalImgWidth / originalImgHeight
     
-        imgWidth = int(self.w * 0.72)
-        imgHeight = int(imgWidth / imgProportion)
+        imgWidth = self.w - 200 # int(self.w * 0.72)
+        imgHeight = self.h - 200 #int(imgWidth / imgProportion)
         
         resizedImg = originalImg.resize((imgWidth , imgHeight),Image.ANTIALIAS)
         self.img = ImageTk.PhotoImage(resizedImg)
         
-        Tkinter.Label(self.root, image = self.img).grid(row=0, column=0)
+        Tkinter.Label(self.root, image = self.img).place(x=5, y=5)
         #img.grid(row=0, column=0, sticky=W+E+N+S, padx=5, pady=5)
     
     def loadButtons(self):
     
-        Tkinter.Label(self.root, text="Lorem Ipsum").grid(row=0, column=1)
+        Tkinter.Label(self.root, text="Lorem Ipsum").place(x=self.w - 180, y= 10)
         
-        Tkinter.Button(self.root, text="Take Picture", command = self.takePicture).grid(row=1, column=0)
+        Tkinter.Button(self.root, text="Take Picture", command = self.takePicture).place(x= 20, y= self.h-180)
     
-        Tkinter.Button(self.root, text="Discard Picture").grid(row=3, column=0)
+        Tkinter.Button(self.root, text="Discard Picture").place(x= 20, y= self.h-140)
     
-        Tkinter.Button(self.root, text="Upload Picture", command = self.uploadPicture).grid(row=2, column=0)
+        Tkinter.Button(self.root, text="Upload Picture", command = self.uploadPicture).place(x= 20, y= self.h-100)
 
     
     def takePicture(self):
